37137c139b805570f373812fee80b9073cec939c
Harsh2504 <harsh2504patil@gmail.com>
2024-02-12 13:14:41 +0530

    navbar v3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import "./Navbar.css"
 export default function Navbar() {
-  return (
-    <div className='navbar bord'>
-        <div>
-            {/* <img src=''></img> */}
-            <h3>Blood Donation</h3>
+    return (
+        <div className='navbar bord'>
+            <div className='logo bord'>
+                {/* <img src=''></img> */}
+                <h3>Blood Donation</h3>
+            </div>
+            <div className='navtabs bord'>
+                <Link  to={"/"}><p>Home</p></Link>
+                <Link to={"/about"}><p>About</p></Link>
+                <Link to='/feedback'><p>Feedback</p></Link>
+            </div>
+            <div className='logintab bord'> <Link to='/login'><p>Login</p></Link></div>
+           
         </div>
-        <div className=''></div>
-        <Link to={"/"}><p>Home</p></Link>       
-         <Link to={"/about"}><p>About</p></Link>
-          <Link to='/feedback'><p>Feedback</p></Link>  
-          <div className='logintab'></div> 
-          <Link to='/login'><p>Login</p></Link>   
-    </div>
-  )
+    )
 }
